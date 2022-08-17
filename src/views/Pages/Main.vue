cdac43f623c77454b51c2b848ef71b69ddf10453
bram2507 <abrahamalvarezgomez2507@gmail.com>
2022-08-17 21:51:02 +0200

    feat: second part of restaurant

--- src/views/Pages/Main.vue
+++ src/views/Pages/Main.vue
@@ -11,12 +11,20 @@
 				<section class="main_contanier__call-action__right--subtitle">
 					<h2>Feel the taste of the japanes food from anywhere and anytime.</h2>
 				</section>
+				<section class="main_contanier__call-action__right--buttons">
+					<div class="buttons__action">
+						<button class="buttons__action_style">Order now</button>
+						<button class="buttons__action_style">Groups</button>
+					</div>
+				</section>
 				<section class="main_contanier__call-action_right--customers-says">
 					<div class="customers-says__number">
 						<div>
 							<h2><strong class="number">24K+ </strong></h2>
 						</div>
-						<div><h2>&nbsp;Happy Customers</h2></div>
+						<div>
+							<h2>&nbsp;Happy Customers</h2>
+						</div>
 					</div>
 					<div class="customers-says__number__testimonies">
 						<div class="testemonies--img">
@@ -35,14 +43,25 @@
 		<div class="main_contanier__call-action">
 			<section class="main_contanier__dished">
 				<div class="main_contanier__dished--left">
-					<div>
+					<section>
 						<img src="@/assets/Recurso_18.svg" alt="" />
-					</div>
-					<div>
+					</section>
+					<section>
 						<img src="@/assets/Recurso_4.svg" alt="" />
-					</div>
+					</section>
 				</div>
-				<div class="main_contanier__dished--rigth"></div>
+				<div class="main_contanier__dished--rigth">
+					<section class="main_contanier__dished--rigth__title">
+						<h1>Our mission is to bring true Japanese flavours to you.</h1>
+					</section>
+					<section class="main_contanier__dished--rigth__subtitle">
+						<h2>
+							We will continue to provide you the experience of Omotenashi, the
+							japanes mindset of hospitatily, with our shopping and dinning for
+							customers.
+						</h2>
+					</section>
+				</div>
 			</section>
 		</div>
 	</div>
@@ -103,7 +122,7 @@ export default {
 	width: 75%;
 	min-height: 50vh;
 	background-color: var(--white);
-	flex-direction: column;
+	flex-wrap: wrap;
 }
 
 .main_contanier__call-action__right--title {
@@ -126,6 +145,12 @@ export default {
 	margin-left: 2vh;
 	width: 100%;
 	min-height: 5vh;
+	margin-bottom: 5vh;
+}
+
+.main_contanier__call-action__right--subtitle h2 {
+	font-size: 2vw;
+	padding: 2vh;
 }
 
 .main_contanier__call-action__right--title h1 {
@@ -156,6 +181,10 @@ export default {
 	border-left: 3px solid var(--black);
 	padding: 2vh;
 }
+.customers-says__number > div {
+	margin-top: 2vh;
+	margin-bottom: 4vh;
+}
 
 .customers-says__number h1 {
 	color: var(--grey);
@@ -168,24 +197,72 @@ export default {
 	font-family: "Noto Sans", sans-serif;
 }
 
+.main_contanier__call-action__right--buttons {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	min-height: 5vh;
+	margin-bottom: 5vh;
+}
+.buttons__action {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 400px;
+	min-height: 5vh;
+}
+.buttons__action_style {
+	width: 150px;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 4vh;
+	outline: none;
+	border: none;
+	border: 2px solid var(--black);
+	background-color: var(--black);
+	color: var(--white);
+	font-size: 1em;
+	transition: 0.5s ease-in-out;
+	margin-left: 2vh;
+	margin-right: 2vh;
+}
+
+.buttons__action_style:hover {
+	width: 150px;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 4vh;
+	outline: none;
+	border: 2px solid var(--black);
+	background-color: var(--white);
+	color: var(--black);
+	font-size: 1em;
+	transition: 0.5s ease-in-out;
+}
+
 .number {
-	font-size: 4em;
+	font-size: 4vw;
 }
 
 .customers-says__number__testimonies {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	width: 400px;
-	height: 50px;
+	width: 500px;
+	min-height: 50px;
 	margin-top: 2vh;
 	flex-direction: row;
 	padding: 2vh;
 }
 
 .testemonies--img img {
-	width: 75px;
-	height: 75px;
+	width: 100px;
+	height: 100px;
 	margin-right: 2vh;
 	margin-left: 2vh;
 	border-radius: 50%;
@@ -208,6 +285,7 @@ export default {
 	align-items: center;
 	width: 100%;
 	min-height: 25vh;
+	flex-wrap: flex;
 	background-color: var(white);
 }
 
@@ -218,10 +296,10 @@ export default {
 	width: 25%;
 	min-height: 25vh;
 	flex-direction: column;
-	background-color: brown;
+	background-color: var(--white);
 }
 
-.main_contanier__dished--left > div:first-child {
+.main_contanier__dished--left > section:first-child {
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -229,10 +307,14 @@ export default {
 	min-height: 35vh;
 	flex-direction: column;
 	background-color: var(--cream);
-	margin-bottom: 0.8vh;
+	margin-bottom: 1vh;
+	background-image: url("@/assets/letters_foods_1.svg");
+	background-position: right;
+	background-repeat: no-repeat;
+	background-size: 30%;
 }
 
-.main_contanier__dished--left > div:nth-child(n) {
+.main_contanier__dished--left > section:last-child {
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -240,12 +322,21 @@ export default {
 	min-height: 35vh;
 	flex-direction: column;
 	background-color: var(--cream);
+	background-image: url("@/assets/letters_foods_1.svg");
+	background-position: right;
+	background-repeat: no-repeat;
+	background-size: 30%;
 }
 
 .main_contanier__dished--left img {
 	width: 45%;
 	height: 45%;
 	image-rendering: optimizeQuality;
+	transition: 0.5s ease-in-out;
+}
+.main_contanier__dished--left img:hover {
+	transform: scale(1.2);
+	transition: 0.5s ease-in-out;
 }
 
 .main_contanier__dished--rigth {
@@ -253,8 +344,40 @@ export default {
 	justify-content: center;
 	align-items: center;
 	width: 75%;
-	min-height: 71vh;
+	height: auto;
+	background-color: var(--white);
+	flex-direction: column;
+}
 
-	background-color: green;
+.main_contanier__dished--rigth__title {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 80%;
+	height: auto;
+}
+
+.main_contanier__dished--rigth__title h1 {
+	font-size: 4vw;
+	font-family: "Noto Sans", sans-serif;
+	padding: 4vh;
+	text-align: center;
+	text-align-last: center;
+}
+
+.main_contanier__dished--rigth__subtitle {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 65%;
+	height: auto;
+}
+
+.main_contanier__dished--rigth__subtitle h2 {
+	font-size: 1.4vw;
+	font-family: "Noto Sans", sans-serif;
+	padding: 4vh;
+	text-align: left;
+	text-align-last: left;
 }
 </style>
