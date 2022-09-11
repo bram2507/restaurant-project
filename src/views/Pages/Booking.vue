0fc4bb0ff9265e56b9b99e04e1a6a055045bfead
bram2507 <abrahamalvarezgomez2507@gmail.com>
2022-09-11 20:27:21 +0200

    TKYO-6 update boooking schudle and map

--- src/views/Pages/Booking.vue
+++ src/views/Pages/Booking.vue
@@ -1,14 +1,34 @@
 <template>
 	<div class="booking__contanier">
+		<div class="booking__container--where__to__find">
+			<h1>Donde encontrarnos</h1>
+		</div>
 		<div class="map-container">
 			<GoogleMap
 				api-key="AIzaSyBGgxahEVkXdc4d--6wYKYw7i3ZIoMmtss"
-				style="width: 100%; height: 500px"
+				style="width: 100%; height: 800px"
 				:center="center"
 				:zoom="20"
 			>
 				<Marker :options="{ position: center }" />
 			</GoogleMap>
+		</div>
+		<div class="booking__container--where__to__find">
+			<h1>Horarios</h1>
+		</div>
+		<div class="schudle">
+			<div
+				class="schudle__days"
+				v-for="(date, key) in Object.values(schudle)"
+				:key="key"
+			>
+				<div>
+					{{ date.day }}
+				</div>
+				<div>
+					{{ date.time }}
+				</div>
+			</div>
 		</div>
 		<!-- <div class="container">
 			<form>
@@ -48,24 +68,57 @@ export default defineComponent({
 		const center = { lat: 40.43317925777378, lng: -3.7033898463846864 };
 		return { center };
 	},
+	data() {
+		return {
+			schudle: [
+				{ day: "Lunes", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+				{ day: "Martes", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+				{ day: "Miercoles", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+				{ day: "Jueves", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+				{ day: "Viernes", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+				{ day: "Sábado", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+				{ day: "Domingo", time: " 13:00 – 17:00 / 20:00 – 23:30" },
+			],
+		};
+	},
 });
 </script>
 <style scoped>
 .booking__contanier {
 	width: 100%;
-	min-height: 85vh;
+	min-height: 50vh;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	background-color: lightslategrey;
 	flex-direction: column;
 }
+.schudle {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-direction: column;
+	width: 100%;
+	height: auto;
+	background-color: var(--white);
+	padding: 4vh;
+}
 
+.schudle div {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	width: 80%;
+	height: auto;
+	background-color: var(--white);
+	padding: 4vh;
+}
 .map-container {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	width: 75%;
+	width: 100%;
 	height: auto;
 }
 
@@ -74,9 +127,21 @@ export default defineComponent({
 	margin: auto;
 	text-align: center;
 	border-radius: 5px;
-	background-color: #f2f2f2;
+	background-color: var(--white);
 	padding: 20px;
 	width: 50%;
+}
+
+.booking__container--where__to__find {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 50vh;
+	background-color: var(--red);
+	color: var(--white);
+
+	font-size: 3vw;
 }
 
 label {
